b7a307fdee3bab0bcf46369086a4cc96443b420c
Antonio Tropiano <antoniotropiano@gmail.com>
2017-05-31 21:33:05 +0200

    aggiungi script per creare lista couni json file

--- src/initial_cleanup/comuni/comuni.py
+++ src/initial_cleanup/comuni/comuni.py
@@ -5,7 +5,7 @@ import sys
 
 def main():
 
-    df = pd.read_excel("../datasets/elenco-comuni-italiani_variazioni_al_23_aprile_2016.xls")
+    df = pd.read_excel("../../../datasets/elenco-comuni-italiani_variazioni_al_23_aprile_2016.xls")
 
     lista_columns = list(df.keys())
     new_lista_columns = lista_columns[5:7] + lista_columns[9:10] + lista_columns[11:12]
