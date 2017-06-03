1c6548b5724dfd13b0c77d59992b0b519f728f31
Antonio Tropiano <antoniotropiano@gmail.com>
2017-06-03 23:21:42 +0200

    modify script that creates comuni list

--- src/initial_cleanup/comuni/comuni.py
+++ src/initial_cleanup/comuni/comuni.py
@@ -5,20 +5,19 @@ import sys
 
 def main():
 
-    df = pd.read_excel("../../../datasets/elenco-comuni-italiani_variazioni_al_23_aprile_2016.xls")
+    df = pd.read_csv("../../../datasets/elenco-comuni-italiani.csv", delimiter=";", encoding="latin-1")
 
     lista_columns = list(df.keys())
-    new_lista_columns = lista_columns[5:7] + lista_columns[9:10] + lista_columns[11:12]
 
-    df = df[new_lista_columns]
+    df = df[lista_columns]
     df = df.dropna(how="all")
     df = df.fillna('')
     df = df.replace(to_replace="-", value="")
-    df = df.drop(df.index[[7999]])
+
     df_dict = df.to_dict(orient='records')
 
     metadata = dict()
-    metadata["date"] = "26052017"
+    metadata["date"] = "03062017"
     metadata["source"] = "istat"
 
     final_dict = dict()
