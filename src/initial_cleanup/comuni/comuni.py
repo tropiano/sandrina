a893dfc28dddac2c957bb5ac0b75d60897ead0c7
Antonio Tropiano <antoniotropiano@gmail.com>
2017-06-18 22:10:17 +0200

    crea file con lista comuni semplificato (nome comune regione provincia e codici comune)

--- src/initial_cleanup/comuni/comuni.py
+++ src/initial_cleanup/comuni/comuni.py
@@ -30,6 +30,32 @@ def main():
         data_towrite = js.dumps(final_dict, outfile, indent=4, ensure_ascii=False)
         outfile.write(unicode(data_towrite))
 
+    minimal_feat = ['Denominazione in italiano','Denominazione in tedesco','Denominazione regione',
+                    'Denominazione provincia',
+                    'Codice Comune numerico con 107 province (dal 2006 al 2009)',
+                    'Codice Comune numerico con 103 province (dal 1995 al 2005)',
+                    'Codice Catastale del comune']
+
+    df = df[minimal_feat]
+    df = df.dropna(how="all")
+    df = df.fillna('')
+    df = df.replace(to_replace="-", value="")
+
+    final_dict = dict()
+
+    df_dict = df.to_dict(orient='records')
+
+    metadata = dict()
+    metadata["date"] = "18062017"
+    metadata["source"] = "istat"
+
+    final_dict["metadata"] = metadata
+    final_dict["data"] = df_dict
+
+    with io.open('../../../json_datasets/comuni/comuni_min.json', 'w', encoding='utf8') as outfile:
+        data_towrite = js.dumps(final_dict, outfile, indent=4, ensure_ascii=False)
+        outfile.write(unicode(data_towrite))
+    
 
 if __name__ == '__main__':
     sys.exit(main())
