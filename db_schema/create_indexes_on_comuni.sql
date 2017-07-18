d54edc1a5d2c90aba667a5501e7b227074460029
Antonio Tropiano <antoniotropiano@gmail.com>
2017-07-18 21:08:15 +0200

    fixa gli script sugli indici. Giuseppe please review.

--- db_schema/create_indexes_on_comuni.sql
+++ db_schema/create_indexes_on_comuni.sql
@@ -1,2 +1,3 @@
-CREATE INDEX comuni_idx ON comuni(codice_catastale, codice_comune_103, codice_comune_107, nome) USING btree;
+\c sandrina 
+CREATE INDEX comuni_idx ON comuni USING btree (codice_catastale, codice_comune_103, codice_comune_107, nome);
 VACUUM ANALYZE comuni;
--- db_schema/create_indexes_on_startup.sql
+++ db_schema/create_indexes_on_startup.sql
@@ -1,3 +1,4 @@
-CREATE INDEX startup_fkey_idx ON startup(anno, mese) USING btree;
-CREATE INDEX on startup(entry jsonb_path_ops) USING gin;
+\c sandrina 
+CREATE INDEX startup_fkey_idx ON startup USING btree(anno, mese);
+CREATE INDEX on startup USING gin(entry jsonb_path_ops);
 VACUUM ANALYZE startup;
