d10885b1a6e57ed2c7084d1e41a6a38a532422f9
Antonio Tropiano <antoniotropiano@gmail.com>
2017-09-02 18:55:26 +0200

    aggiunta view per regioni e view approfondita per Toscana

--- docker/postgresql/fill_db.py
+++ docker/postgresql/fill_db.py
@@ -12,13 +12,35 @@ js_data=js_dict['data']
 conn = psycopg2.connect("dbname='sandrina' user='sandrina' host='postgresql' password='v5sYvBKw'")
 cur = conn.cursor()
 
+id_comune_dict = {}
+
 for j in js_data:
     
-    data_tuple = (j['Codice Catastale del comune'], j['Codice Comune numerico con 103 province (dal 1995 al 2005)'],
+    data_tuple = (j['Codice Comune numerico con 107 province (dal 2006 al 2009)'],
+                  j['Codice Catastale del comune'], j['Codice Comune numerico con 103 province (dal 1995 al 2005)'],
                   j['Codice Comune numerico con 107 province (dal 2006 al 2009)'], j['Denominazione in italiano'],
                   j['Denominazione regione'])
-                  
-    cur.execute("INSERT INTO comuni(codice_catastale, codice_comune_103, codice_comune_107, nome, regione) VALUES (%s,%s,%s,%s,%s)", data_tuple)
+            
+    #build dict id comune 
+    id_comune_dict[ j['Denominazione in italiano'] ] = j['Codice Comune numerico con 107 province (dal 2006 al 2009)']
+              
+    cur.execute("INSERT INTO comuni(id, codice_catastale, codice_comune_103, codice_comune_107, nome, regione) VALUES (%s,%s,%s,%s,%s,%s)", data_tuple)
     
     
+#load the salute json file
+f = open("/var/lib/postgresql/salute.json")
+js_file = f.read()
+js_dict = js.loads(js_file,encoding='utf-8')
+js_data=js_dict['data']
+
+for j in js_data:
+    
+    try:
+        comune_id = id_comune_dict[ j['comune'] ]
+        data_tuple = (comune_id, j['year'], js.dumps(j))
+
+        cur.execute("INSERT INTO salute(comune_id, anno, entry) VALUES (%s,%s,%s)", data_tuple)
+    except:
+        print j['comune'], " not found"
+        continue
 conn.commit()
--- docker/web/app.py
+++ docker/web/app.py
@@ -1,6 +1,8 @@
 import time
 from flask import Flask, render_template, flash, redirect, request, url_for
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+
 
 
 DBUSER = 'sandrina'
@@ -44,14 +46,46 @@ def home():
     return render_template('home_datum.html', entries=cur)
 
 
-@app.route('/<nome_regione>/comuni', methods=['GET'])
-def show_comuni(nome_regione):
+@app.route('/regioni', methods=['GET'])
+def show_comuni():
     
-    cur=db.session.query(comuni).filter(comuni.regione == nome_regione)
-    return render_template('analytics_datum.html', entries=cur.all(), regione=nome_regione)
+    cur = db.session.query(comuni.regione, func.count(comuni.id).label("n_comuni")).group_by(
+          comuni.regione).order_by(func.count(comuni.id).label("n_comuni").desc())
+#    cur_1 = db.session.query(comuni, salute).join(salute, salute.comune_id==comuni.id).filter(comuni.regione == nome_regione)
+    
+    return render_template('analytics_datum.html', entries=cur.all())
 
 
+@app.route('/<nome_regione>', methods=['GET'])
+def show_regione(nome_regione):
+    
+    cur = db.session.query(comuni).filter(comuni.regione == nome_regione)
+    
+    cur_1 = db.session.query(comuni, salute).join(salute, salute.comune_id==comuni.id).filter(
+            comuni.regione == nome_regione).filter(salute.anno == 2014)
+    
+    negri = []
+    
+    print cur_1.all()
+    
+    for datum in cur_1.all():
+        
+        j_datum = datum[1].entry 
+        comune_name = datum[0].nome
+        negri.append((j_datum['Percentuale nati di cittadinanza non italiana'], comune_name))
+    
+    negri_max = max(negri, key=lambda item:item[0])
+    negri_min = min(negri, key=lambda item:item[0])
+    negri_avg = sum(n[0] for n in negri)/float(len(negri))
+    
+    return render_template('analytics_regioni_datum.html', entries=cur.all(), regione = nome_regione,
+                           negri_max = negri_max, negri_min = negri_min, negri_avg = negri_avg)
 
+    #cur = db.session.query(Teams, Leagues, Results).join(Results,Teams.id==Results.team_id).join(
+    #      Leagues, Leagues.id == Teams.league_id).filter(Leagues.league_name == serie_name).filter(
+    #      Results.fixture == fixture).filter(Leagues.season == season_name).group_by(
+    #      Results.team_id).order_by(Results.points.desc())
+    
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
